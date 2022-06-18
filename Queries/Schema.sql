3fea56e0bf9af42ee38d919663cbeb5580fca01e
sesan ogunturoti <mygoodwill@yahoo.com>
2022-06-17 20:49:37 -0400

    Adding Pewlett-Hackard-Analysis

--- Queries/Schema.sql
+++ Queries/Schema.sql
@@ -31,4 +31,37 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+CREATE TABLE titles (
+emp_no INT NOT NULL,
+	title varchar(40),
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+); 
+
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    		PRIMARY KEY (emp_no, dept_no, from_date)
+);
+
 SELECT * FROM departments;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
